e7addf9bd6a102542308ae42939a56a2e3ca38dc
Aditya Narayan <adityanarayan29j@gmail.com>
2024-03-24 00:16:27 +0400

    feat: Added Calender component

--- src/ActivityCalenderComponent.jsx
+++ src/ActivityCalenderComponent.jsx
@@ -7,7 +7,7 @@ const ActivityCalendarComponent = () => {
   const data = [
     { date: '2024-03-19', count: 5 },
     { date: '2024-03-20', count: 7 },
-    { date: '2024-03-21', count: 3 },
+    { date: '2024-03-21', count: 5 },
     // Add more data as needed
   ];
 
